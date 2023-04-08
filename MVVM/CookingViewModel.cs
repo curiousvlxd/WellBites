350983a69fef61385d2a2238a211655a8b735fc0
anti79 <anti7q@gmail.com>
2023-04-08 18:03:39 +0300

    added core mvvm classes

---
+++ MVVM/CookingViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Org.OpenAPITools.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WellBites.Models;
+
+namespace WellBites.MVVM
+{
+	internal class CookingViewModel
+	{
+		List<Ingredient> suggestedIngredients;
+
+
+		
+	}
+}
