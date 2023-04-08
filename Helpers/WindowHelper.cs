c4785cdd4b746ebbb45df11bb5e4f1a8ac7d53bd
vlxdtimchenko <vlxdtimchenko@gmail.com>
2023-04-08 13:51:57 +0300

    Initial Commit

---
+++ Helpers/WindowHelper.cs
@@ -0,0 +1,310 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using System.Runtime.InteropServices;
+using System.Windows.Interop;
+using System.Drawing.Printing;
+
+namespace WellBites.Helpers
+{
+    public static class WindowHelper
+    {
+        public static bool isMax { get; set; } = false;
+        public static bool isFull { get; set; } = false;
+        public static Point old_loc { get; set; }
+        public static Point default_loc { get; set; }
+        public static Size old_size { get; set; }
+        public static Size default_size { get; set; }
+        public static Window _this { get; set; }
+
+        [DllImport("user32")]
+        internal static extern bool GetMonitorInfo(IntPtr hMonitor, MONITORINFO lpmi);
+
+        [DllImport("User32")]
+        internal static extern IntPtr MonitorFromWindow(IntPtr handle, int flags);
+
+        static public void win_SourceInitialized(object sender, EventArgs e)
+        {
+            System.IntPtr handle = (new System.Windows.Interop.WindowInteropHelper(_this)).Handle;
+            System.Windows.Interop.HwndSource.FromHwnd(handle).AddHook(new System.Windows.Interop.HwndSourceHook(WindowProc));
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct MINMAXINFO
+        {
+            public POINT ptReserved;
+            public POINT ptMaxSize;
+            public POINT ptMaxPosition;
+            public POINT ptMinTrackSize;
+            public POINT ptMaxTrackSize;
+        };
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct POINT
+        {
+            public int x;
+            public int y;
+
+            public POINT(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+        }
+        private static System.IntPtr WindowProc(System.IntPtr hwnd, int msg, System.IntPtr wParam, System.IntPtr lParam, ref bool handled)
+        {
+            switch (msg)
+            {
+                case 0x0024:
+                    WmGetMinMaxInfo(hwnd, lParam);
+                    handled = true;
+                    break;
+            }
+
+            return (System.IntPtr)0;
+        }
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+        public class MONITORINFO
+        {
+
+            public int cbSize = Marshal.SizeOf(typeof(MONITORINFO));
+
+            public RECT rcMonitor = new RECT();
+
+            public RECT rcWork = new RECT();
+
+            public int dwFlags = 0;
+        }
+
+        [StructLayout(LayoutKind.Sequential, Pack = 0)]
+        public struct RECT
+        {
+            public int left;
+            public int top;
+            public int right;
+            public int bottom;
+
+            public static readonly RECT Empty = new RECT();
+
+            public int Width
+            {
+                get { return Math.Abs(right - left); }
+            }
+
+            public int Height
+            {
+                get { return bottom - top; }
+            }
+
+            public RECT(int left, int top, int right, int bottom)
+            {
+                this.left = left;
+                this.top = top;
+                this.right = right;
+                this.bottom = bottom;
+            }
+
+            public RECT(RECT rcSrc)
+            {
+                this.left = rcSrc.left;
+                this.top = rcSrc.top;
+                this.right = rcSrc.right;
+                this.bottom = rcSrc.bottom;
+            }
+
+            public bool IsEmpty
+            {
+                get
+                {
+                    return left >= right || top >= bottom;
+                }
+            }
+
+            public override bool Equals(object obj)
+            {
+                if (!(obj is Rect)) { return false; }
+                return (this == (RECT)obj);
+            }
+
+            public override int GetHashCode()
+            {
+                return left.GetHashCode() + top.GetHashCode() + right.GetHashCode() + bottom.GetHashCode();
+            }
+
+
+            public static bool operator ==(RECT rect1, RECT rect2)
+            {
+                return (rect1.left == rect2.left && rect1.top == rect2.top && rect1.right == rect2.right && rect1.bottom == rect2.bottom);
+            }
+
+            public static bool operator !=(RECT rect1, RECT rect2)
+            {
+                return !(rect1 == rect2);
+            }
+        }
+        private static void WmGetMinMaxInfo(System.IntPtr hwnd, System.IntPtr lParam)
+        {
+
+            MINMAXINFO mmi = (MINMAXINFO)Marshal.PtrToStructure(lParam, typeof(MINMAXINFO));
+
+            int MONITOR_DEFAULTTONEAREST = 0x00000002;
+            System.IntPtr monitor = MonitorFromWindow(hwnd, MONITOR_DEFAULTTONEAREST);
+
+            if (monitor != System.IntPtr.Zero)
+            {
+                MONITORINFO monitorInfo = new MONITORINFO();
+                GetMonitorInfo(monitor, monitorInfo);
+                RECT rcWorkArea = monitorInfo.rcWork;
+                RECT rcMonitorArea = monitorInfo.rcMonitor;
+                mmi.ptMaxPosition.x = Math.Abs(rcWorkArea.left - rcMonitorArea.left);
+                mmi.ptMaxPosition.y = Math.Abs(rcWorkArea.top - rcMonitorArea.top);
+                mmi.ptMaxSize.x = Math.Abs(rcWorkArea.right - rcWorkArea.left);
+                mmi.ptMaxSize.y = Math.Abs(rcWorkArea.bottom - rcWorkArea.top);
+            }
+
+            Marshal.StructureToPtr(mmi, lParam, true);
+        }
+        public static void SetInitials(Window win)
+        {
+            old_size = new Size(win.Width, win.Height);
+            old_loc = new Point(win.Top, win.Left);
+            default_size = new Size(win.Width, win.Height);
+            default_loc = new Point(win.Top, win.Left);
+        }
+
+        public static void Fullscreen(Window win)
+        {
+            if (win.WindowState == WindowState.Normal)
+            {
+                win.WindowState = WindowState.Maximized;
+            }
+            else win.WindowState = WindowState.Normal;
+        }
+
+        public static void DoMaximize(Window win)
+        {
+            if (isMax == false)
+            {
+                old_size = new Size(win.Width, win.Height);
+                old_loc = new Point(win.Top, win.Left);
+                Maximize(win);
+                isMax = true;
+            }
+            else
+            {
+                win.Top = old_loc.Y;
+                win.Left = old_loc.X;
+                win.Width = old_size.Width;
+                win.Height = old_size.Height;
+                isMax = false;
+                isFull = false;
+            }
+        }
+        public static void DoFullscreen()
+        {
+            if (isMax == false)
+            {
+                old_size = new Size(_this.Width, _this.Height);
+                old_loc = new Point(_this.Top, _this.Left);
+                Fullscreen(_this);
+                isMax = false;
+                isFull = true;
+            }
+            else
+            {
+                _this.Top = old_loc.Y;
+                _this.Left = old_loc.X;
+                _this.Width = old_size.Width;
+                _this.Height = old_size.Height;
+                isMax = false;
+                isFull = false;
+            }
+        }
+        public static void Maximize(Window win)
+        {
+            double x = SystemParameters.WorkArea.Width;
+            double y = SystemParameters.WorkArea.Height;
+            win.WindowState = WindowState.Normal;
+            win.Top = 0;
+            win.Left = 0;
+            win.Width = x;
+            win.Height = y;
+        }
+        public static void Minimize()
+        {
+            _this.WindowState = WindowState.Minimized;
+        }
+        public static void Exit()
+        {
+            Application.Current.Shutdown();
+        }
+
+        public static void MoveWindow(MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                _this.DragMove();
+            }
+        }
+
+        [DllImport("dwmapi.dll", PreserveSig = true)]
+        private static extern int DwmSetWindowAttribute(IntPtr hwnd, int attr, ref int attrValue, int attrSize);
+
+        [DllImport("dwmapi.dll")]
+        private static extern int DwmExtendFrameIntoClientArea(IntPtr hWnd, ref Margins pMarInset);
+
+        /// <summary>   
+        /// Drops a standard shadow to a WPF Window, even if the window is borderless. Only works with DWM (Windows Vista or newer).
+        /// This method is much more efficient than setting AllowsTransparency to true and using the DropShadow effect,
+        /// as AllowsTransparency involves a huge performance issue (hardware acceleration is turned off for all the window).
+        /// </summary>
+        /// <param name="window">Window to which the shadow will be applied</param>
+        public static void DropShadowToWindow(Window window)
+        {
+            if (!DropShadow(window))
+            {
+                window.SourceInitialized += new EventHandler(window_SourceInitialized);
+            }
+        }
+
+        private static void window_SourceInitialized(object sender, EventArgs e)
+        {
+            Window window = (Window)sender;
+
+            DropShadow(window);
+
+            window.SourceInitialized -= new EventHandler(window_SourceInitialized);
+        }
+
+        /// <summary>
+        /// The actual method that makes API calls to drop the shadow to the window
+        /// </summary>
+        /// <param name="window">Window to which the shadow will be applied</param>
+        /// <returns>True if the method succeeded, false if not</returns>
+        private static bool DropShadow(Window window)
+        {
+            try
+            {
+                WindowInteropHelper helper = new WindowInteropHelper(window);
+                int val = 2;
+                int ret1 = DwmSetWindowAttribute(helper.Handle, 2, ref val, 4);
+
+                if (ret1 == 0)
+                {
+                    Margins m = new Margins { Bottom = 0, Left = 0, Right = 0, Top = 0 };
+                    int ret2 = DwmExtendFrameIntoClientArea(helper.Handle, ref m);
+                    return ret2 == 0;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using WellBites.Themes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WellBites.Helpers;
+
+namespace WellBites
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            WindowHelper._this = this;
+            this.SourceInitialized += new EventHandler(WindowHelper.win_SourceInitialized);
+        }
+
+        private void OnThemesClick(object sender, RoutedEventArgs e)
+        {
+            ThemesController.SetTheme(ThemesToggleButton.IsChecked == true
+                ? ThemesController.ThemeTypes.Dark
+                : ThemesController.ThemeTypes.Light);
+        }
+
+        private void OnBtnCloseClick(object sender, RoutedEventArgs e)
+        {
+            WindowHelper.Exit();
+        }
+
+        private void OnBtnFullScreenClick(object sender, RoutedEventArgs e)
+        {
+            WindowHelper.DoFullscreen();
+        }
+
+        private void OnBtnMinimizeClick(object sender, RoutedEventArgs e)
+        {
+            WindowHelper.Minimize();
+        }
+
+        private void OnTitleBarMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            WindowHelper.MoveWindow(e);
+        }
+
+        private void OnWindowSizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            if (this.Height == 800 && this.Width == 1000)
+            {
+                if (this.WindowState == WindowState.Maximized) BorderMain.Margin = new Thickness(0);
+                else BorderMain.Margin = new Thickness(30);
+            }
+            else BorderMain.Margin = new Thickness(0);
+        }
+    }
+}
