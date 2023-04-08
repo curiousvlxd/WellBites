e4642bd1868906509513532bdeb62645e2202708
anti79 <anti7q@gmail.com>
2023-04-08 16:32:42 +0300

    basic user model

---
+++ Models/UserCharacteristics.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WellBites.Models
+{
+	public enum Sex
+	{
+		Male,
+		Female
+	}
+	public enum Activity
+	{
+		BMR, //bare minimum needed to stay alive
+		Sedentary, //no exercise 
+		Light, //1-3 times/week
+		Moderate, //4-5 times/week
+		Active, //daily exercise or intense exercise 3-4 times/week
+		VeryActive, //intense exercise 6-7 times/week
+		ExtraActive //very intense exercise daily or physical job
+	}
+	internal class UserCharacteristics
+	{	
+		public int Id { get; set; }
+		public int Weight { get; set; }
+		public int Height { get; set; }
+		public Sex Sex { get; set; }
+
+		public 
+
+	}
+}
