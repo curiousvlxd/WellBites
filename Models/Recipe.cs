fc1e6d7e80c4ace19ec301be239673bb661f4893
anti79 <anti7q@gmail.com>
2023-04-09 15:14:39 +0300

    fix fav

--- Models/Recipe.cs
+++ Models/Recipe.cs
@@ -43,7 +43,7 @@ namespace WellBites.Models
             set
             {
                 isFavorite = value;
-                //OnPropertyChanged();
+                OnPropertyChanged();
             }
         }
         public string ImageURL
