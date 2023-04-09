c24f8b17f34ea1b2fbf7e438ee6902a096955f5b
vlxdtimchenko <vlxdtimchenko@gmail.com>
2023-04-09 12:06:46 +0300

    Changed burger menu

---
+++ Migrations/20230409084155_InitialTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WellBites.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialTest : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
